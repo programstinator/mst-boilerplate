6b2feb4a8f6fa10bea3512e800d0b07ebf1bc17d
programstinator <programstinator@protonmail.com>
2022-06-30 19:25:05 +0200

    Chart example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import { observer } from 'mobx-react';
 import { useMst } from '../internal';
-import Chart from '@chartiq/react-components';
+import MyChart from './MyChart';
 export const HomePage = observer(() => {
   const {
     router: { currentView },
   } = useMst();
-  return <Chart config={{ initialSymbol: 'FB' }} />;
+  return (
+    <>
+      <MyChart />
+    </>
+  );
 });
